7cd55d08f4906e9e29660419ca6e2267fd78d822
harbor <hluo@bdna.com>
2018-11-29 15:19:56 +0800

    add web socket feature'

---
+++ web-ui-service/src/main/java/com/cloud/web/ui/config/WebSocketConfig.java
@@ -0,0 +1,31 @@
+package com.cloud.web.ui.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ * Created by harbor on 11/29/2018.
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+//public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer  {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/log-web-socket")
+                .setAllowedOrigins("*")
+                .withSockJS();
+    }
+
+}
--- web-ui-service/src/main/java/com/cloud/web/ui/controller/LogController.java
+++ web-ui-service/src/main/java/com/cloud/web/ui/controller/LogController.java
@@ -3,6 +3,8 @@ package com.cloud.web.ui.controller;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -14,12 +16,17 @@ public class LogController {
 
     private static final Logger logger = LoggerFactory.getLogger(LogController.class);
 
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
     @PostMapping("/receive/{topicName}")
     @ApiOperation(value = "Receive log from message service", notes ="Receive log from message service")
     public String receiveLog(@PathVariable("topicName") String topicName,
                              @RequestBody String message){
 
         logger.info("Receive message: topic={} message:{}",topicName, message);
+
+        messagingTemplate.convertAndSend("/log/topic/"+topicName, message);
 
         return "Done";
     }
