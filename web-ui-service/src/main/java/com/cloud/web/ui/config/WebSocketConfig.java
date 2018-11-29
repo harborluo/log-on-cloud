fc12a0650619126b7ea619645893784b4d1fb7c0
harbor <hluo@bdna.com>
2018-11-29 15:58:00 +0800

    update destination prefix

--- web-ui-service/src/main/java/com/cloud/web/ui/config/WebSocketConfig.java
+++ web-ui-service/src/main/java/com/cloud/web/ui/config/WebSocketConfig.java
@@ -17,8 +17,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.setApplicationDestinationPrefixes("/log");
         config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/");
     }
 
     @Override
