cd96878f1098bcb017bf8d35a0054577be3dd4c3
harbor <hluo@bdna.com>
2018-12-05 14:11:24 +0800

    add log for send message to Dingtalk

--- notification-service/src/main/java/com/cloud/notification/controller/NotificationController.java
+++ notification-service/src/main/java/com/cloud/notification/controller/NotificationController.java
@@ -2,6 +2,8 @@ package com.cloud.notification.controller;
 
 import com.cloud.notification.service.DingTalkService;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,9 +21,14 @@ public class NotificationController {
     @Autowired
     DingTalkService service;
 
+    private static final Logger logger = LoggerFactory.getLogger(NotificationController.class);
+
     @PostMapping(value = "/send/message/dev")
     @ApiOperation(value = "Send message to dev group", notes ="Send message to dev group via DingTalk")
     public @ResponseBody ResponseEntity<String> sendMessage2Dev(@RequestBody String message){
+
+        logger.info("Send message to dev group: \n {}", message);
+
         String body = service.sendMessage2Dev(message);
         ResponseEntity<String>  response = new ResponseEntity<>(body, HttpStatus.OK);
         return response;
@@ -31,6 +38,7 @@ public class NotificationController {
     @ApiOperation(value = "Send message to dev group", notes ="Send message to researcher group via DingTalk")
     public @ResponseBody ResponseEntity<String> sendMessage2Researcher(@RequestBody String message){
         String body = service.sendMessage2Researcher(message);
+        logger.info("Send message to researcher group: \n {}", message);
         ResponseEntity<String>  response = new ResponseEntity<>(body, HttpStatus.OK);
         return response;
     }
